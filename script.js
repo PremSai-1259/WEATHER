fac1915b6acaa3270cba675a3d629be287ccfc17
PremSai-1259 <kps483624@gmail.com>
2025-03-19 16:34:58 +0530

    ADDING DETAILS..

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ async function infor()
         alert("PLEASE ENTER CITY");
     }
     else{
-        var url = await fetch(`https://api.weatherapi.com/v1/current.json?q=${city}&key=3c004e1464a84e97bb5181340251603`);
-        var result = await url.json();
+        var response= await fetch(`https://api.weatherapi.com/v1/current.json?q=${city}&key=3c004e1464a84e97bb5181340251603`);
+        var result = await response.json();
         document.getElementById("time").innerHTML=result.location.localtime;
         document.getElementById("temp").innerHTML=`${result.current.temp_c } C`;
         document.getElementById("windvel").innerHTML=`${result.current.wind_mph} mph`;
