b478dea8d9f55b16b969a51e2c2bbfb561d178e1
PremSai-1259 <kps483624@gmail.com>
2025-03-19 15:10:53 +0530

    CONVERSION BUTTONS

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ async function infor()
         alert("PLEASE ENTER CITY");
     }
     else{
-        var url = await fetch('https://api.weatherapi.com/v1/current.json?q=${city}&key=3c004e1464a84e97bb5181340251603');
+        var url = await fetch(`https://api.weatherapi.com/v1/current.json?q=${city}&key=3c004e1464a84e97bb5181340251603`);
         var result = await url.json();
         document.getElementById("time").innerHTML=result.location.localtime;
-        document.getElementById("temp").innerHTML=result.current.temp_c +"C";
-        document.getElementById("windvel").innerHTML=result.current.wind_mph +"mph";
+        document.getElementById("temp").innerHTML=`${result.current.temp_c } C`;
+        document.getElementById("windvel").innerHTML=`${result.current.wind_mph} mph`;
+    }
+    function ct(){
+        if(result)
+        document.getElementById("temp").innerHTML=`${result.current.temp_f}F`;
+        else{
+            alert("ENTER THE CITY NAME")
+        }
+    }
+    function cw(){
+        if(result)
+        document.getElementById("windvel").innerHTML=`${result.current.wind_kph} kph`;
+        else{
+            alert("ENTER THE CITY NAME")
+        }
     }
 }
 
-function ct(){
-    document.getElementById("temp").innerHTML=result.current.temp_f +"F";
-}
-function cw(){
-    document.getElementById("windvel").innerHTML=result.current.wind_kph +"kph";
-}
