dbee5e295d9cdec8c06e8d0c66e4a3e445abc52c
PremSai-1259 <kps483624@gmail.com>
2025-03-19 14:28:18 +0530

    FIXING

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 async function infor()
 {
     var city=document.getElementById("place").value;
-    var key= "07ebb815f611499f808202728242304";
+    var key= "3c004e1464a84e97bb5181340251603";
     if(city==""){
         alert("PLEASE ENTER CITY");
     }
     else{
-        var url = await fetch('https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}');
+        var url = await fetch('http://api.weatherapi.com/v1/current.json?key=3c004e1464a84e97bb5181340251603&q=${city}&aqi=yes');
         var result = await url.json();
-        document.getElementById("time").innerHTML=result.current.time;
-        document.getElementById("temp").innerHTML=result.current.temp_c;
-        document.getElementById("windvel").innerHTML=result.current.wind_mph;
+        document.getElementById("time").innerHTML=result.location.localtime;
+        document.getElementById("temp").innerHTML=result.current.temp_c +"C";
+        document.getElementById("windvel").innerHTML=result.current.wind_mph +"mph";
     }
 }
